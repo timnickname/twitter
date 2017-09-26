ca20fe4b693dd7b65bad9a244fb135be08b44c76
BuildTools <unconfigured@null.spigotmc.org>
2017-09-27 00:53:25 +0200

    initial commit

---
+++ functions.php
@@ -0,0 +1,6 @@
+function add_post($userid,$body){
+    $sql = "insert into posts (user_id, body, stamp)
+            values ($userid, '". mysql_real_escape_string($body). "',now())";
+
+    $result = mysql_query($sql);
+}
---
+++ header.php
@@ -0,0 +1,11 @@
+$SERVER = 'localhost';
+$USER = 'root';
+$PASS = '';
+$DATABASE = 'twitter'
+
+if (!($mylink = mysql_connect( $SERVER, $USER, $PASS))) {
+    echo  "<h3>Sorry, could not connect to database.</h3><br/>
+    Please contact admin(at)domain.com for more help\n";
+    exit;
+}
+mysql_select_db($DATABASE);

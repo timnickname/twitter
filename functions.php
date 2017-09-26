d7be7165fab2a41b9e3fe1519c082aab9056b72b
timnickname <unconfigured@null.spigotmc.org>
2017-09-27 01:08:06 +0200

    edit 0.1

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
+<?php
 function add_post($userid,$body){
     $sql = "insert into posts (user_id, body, stamp)
             values ($userid, '". mysql_real_escape_string($body). "',now())";
 
     $result = mysql_query($sql);
 }
+?>
--- header.php
+++ header.php
@@ -1,11 +1,13 @@
+<?php
 $SERVER = 'localhost';
 $USER = 'root';
 $PASS = '';
-$DATABASE = 'twitter'
+$DATABASE = 'twitter';
 
-if (!($mylink = mysql_connect( $SERVER, $USER, $PASS))) {
+if (!($mylink = mysql_connect( $SERVER, $USER, $PASS, $DATABASE))) {
     echo  "<h3>Sorry, could not connect to database.</h3><br/>
     Please contact admin(at)domain.com for more help\n";
     exit;
 }
 mysql_select_db($DATABASE);
+?>
